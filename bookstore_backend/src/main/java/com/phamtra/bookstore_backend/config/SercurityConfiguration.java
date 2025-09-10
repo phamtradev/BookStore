3f3c27148aaaafd9966397a907d0c3af7b364aa9
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-09-10 11:08:39 +0700

    feat: config cors configuration

---
+++ bookstore_backend/src/main/java/com/phamtra/bookstore_backend/config/SercurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.phamtra.bookstore_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+public class SercurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //cấu hình security
+
+//        String[] whiteList = {
+//                "/",
+//                "/api/v1/auth/login", "/api/v1/auth/refresh", "api/v1/auth/register",
+//                "/storage/**"
+//        };
+
+        http
+                .csrf(c -> c.disable())
+                .cors(Customizer.withDefaults()); //cấu hình mặc định lỗi cors
+//                .authorizeHttpRequests(
+//                        authz -> authz
+//                                .requestMatchers(whiteList).permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/api/v1/companies").permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/api/v1/jobs").permitAll()
+//                                .requestMatchers(HttpMethod.GET, "/api/v1/skills").permitAll()
+//                                .anyRequest().authenticated())
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults())
+//                        .authenticationEntryPoint(customAuthenicationEntryPoint)
+//                )
+        //lỗi ngoại lệ
+//                .exceptionHandling(
+//                        exceptions -> exceptions
+//                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401
+//                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
+
+//                .formLogin(f -> f.disable())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
